86698a6ff013ac5f525af43a9606d11378339df8
tatadavila <tatdev91@gmail.com>
2022-02-16 13:59:29 -0500

    Modify loader

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,18 +1,18 @@
 import React from "react";
-import Backdrop from '@mui/material/Backdrop';
-import CircularProgress from '@mui/material/CircularProgress';
+import Backdrop from "@mui/material/Backdrop";
+import CircularProgress from "@mui/material/CircularProgress";
 
-const Loader = () => {
+const Loader = ({show}) => {
   return (
-    <div>
+    <>
       <Backdrop
         sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
-        open
+        open={show}
       >
         <CircularProgress color="inherit" />
       </Backdrop>
-    </div>
+    </>
   );
 };
 
-export default Loader
+export default Loader;
