02c04e156284302d0bc5584811c76833210fe7da
tatadavila <tatdev91@gmail.com>
2022-02-16 23:53:02 -0500

    format file

--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,6 @@
 import Header from "./Header";
-import Loader from './Loader'
+import Loader from "./Loader";
 import SearchBox from "./SearchBox";
 import UserList from "./UserList";
 
-export {
-  Header,
-  Loader,
-  SearchBox,
-  UserList,
-};
+export { Header, Loader, SearchBox, UserList };
