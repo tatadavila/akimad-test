1593d249f8ef3a785651c0d5b91d37cdde595eac
tatadavila <tatdev91@gmail.com>
2022-02-17 05:08:15 -0500

    add state to app.js in order to show and hide list

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import { Header, Loader, SearchBox, UserList } from "./components/index";
 
 function App() {
   const [showLoader] = useState(false);
   const [searchValue, setSearchValue] = useState("");
-  const [showUserList] = useState(false);
+  const [showList, setShowList] = useState(false);
+
+  console.log({ searchValue });
+
+  useEffect(() => {
+    searchValue !== "" ? setShowList(true) : setShowList(false);
+  }, [searchValue]);
+
   return (
     <div className="App">
       {showLoader ? <Loader /> : null}
       <Header />
       <SearchBox value={searchValue} setValue={setSearchValue} />
-      <UserList show={showUserList} />
+      <UserList query={searchValue} show={showList} />
     </div>
   );
 }
