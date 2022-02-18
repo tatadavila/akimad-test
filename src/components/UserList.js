f35dc8350bad42d7bed63ae8d41680ef83e1c1e1
tatadavila <tatdev91@gmail.com>
2022-02-17 21:41:44 -0500

    configure list to show users

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,86 +1,57 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+
+import { getUser, authenticate } from "../data/Fetch";
 
 import Avatar from "@mui/material/Avatar";
 import Box from "@mui/material/Box";
 import Divider from "@mui/material/Divider";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
 import ListItemAvatar from "@mui/material/ListItemAvatar";
 import ListItemText from "@mui/material/ListItemText";
 import { Typography } from "@mui/material";
 
-import { FixedSizeList } from "react-window";
+const UserList = ({ show, query, setLoader }) => {
+  const [userData, setUserData] = useState(null);
 
-const data = [
-  "Racing car sprays burning fuel into crowd.",
-  "Japanese princess to wed commoner.",
-  "Australian walks 100km after outback crash.",
-  "Man charged over missing wedding girl.",
-  "Los Angeles battles huge wildfires.",
-];
+  // authenticate();
 
-const UserList = ({show}) => {
-  console.log({show});
+  useEffect(() => {
+    const fetchData = async () => {
+      if (query !== "") {
+        const response = await getUser(query);
+        setUserData(response.data);
+      }
+    };
+    fetchData();
+  }, [query]);
+
+  console.log(userData);
+
   return (
     <div className="listContainer">
-      {show ? (
-        <div>
-          <Box
-            sx={{
-              width: "100%",
-              height: 250,
-              maxWidth: 360,
-              bgcolor: "background.paper",
-            }}
+      {show && userData && (
+        <Box>
+          <List
+            sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}
           >
-            <FixedSizeList
-              height={250}
-              width={360}
-              itemSize={46}
-              itemCount={200}
-              overscanCount={5}
-            >
-              {renderRow}
-            </FixedSizeList>
-          </Box>
-        </div>
-      ) : null}
+            {
+              <ListItem alignItems="flex-start">
+                <ListItemAvatar>
+                  <Avatar alt="Remy Sharp" src={userData.avatar_url} />
+                </ListItemAvatar>
+                {userData.name ? (
+                  <ListItemText primary={userData.name} />
+                ) : (
+                  <ListItemText primary="No Name" />
+                )}
+              </ListItem>
+            }
+          </List>
+        </Box>
+      )}
     </div>
   );
 };
 
 export default UserList;
-
-const renderRow = (props) => {
-  const { index, style } = props;
-
-  return (
-    <>
-      {data &&
-        data.map((user, index) => {
-          <ListItem alignItems="flex-start">
-            <ListItemAvatar>
-              <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
-            </ListItemAvatar>
-            <ListItemText
-              primary="Brunch this weekend?"
-              secondary={
-                <React.Fragment>
-                  <Typography
-                    sx={{ display: "inline" }}
-                    component="span"
-                    variant="body2"
-                    color="text.primary"
-                  >
-                    Ali Connors
-                  </Typography>
-                  {" — I'll be in your neighborhood doing errands this…"}
-                </React.Fragment>
-              }
-            />
-          </ListItem>;
-        })}
-    </>
-  );
-};
